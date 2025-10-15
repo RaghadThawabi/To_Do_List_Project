c4cd7fc6fadc6949a98caf2f3be841c46f51cb58
Raghad <raghadmoh.tha@gmail.com>
2025-10-15 11:18:42 +0300

    New Load file , unique ID , OOP

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import json
+import uuid
+from itertools import count
 
-tasks = []
+#NEW
+class TaskStructure:
+    _id_counter = count(1)
+    def __init__(self,title,description,priority,status="TO-DO",id=None):
+        self.id = id or next(TaskStructure._id_counter)
+        self.title=title
+        self.description=description
+        self.priority=priority
+        self.status=status
+
+
+    def as_dictionary(self):
+        return {
+            "id":self.id,
+            "title":self.title,
+            "description":self.description,
+            "priority":self.priority,
+            "status":self.status,
+        }
 
 
 def menu():
@@ -14,152 +34,158 @@ def menu():
     print("8- Save the list  ")
 
 
-def add_task():
-    while True:
-        title = input("Enter the title of the task: ")
-        if title:
-            break
-        print("Please enter a valid title.")
-    while True:
-        description = input("Enter the description of the task: ")
-        if description:
-            break
-        print("Please enter a valid description.")
-    while True:
-        try:
-            priority = int(input("Enter priority , [highest : 0 - lowest : 5] "))
-            if 0 <= priority <= 5:
+
+#NEW
+class TasksOperations:
+    def __init__(self):
+        self.tasks=self.load_data()
+
+
+    def load_data(self):
+            try:
+                with open("tasks.json", "r") as file:
+                    tasks_from_file = json.load(file)
+                    max_id = max([t["id"] for t in tasks_from_file], default=0)
+                    TaskStructure._id_counter = count(max_id + 1)
+                    return [TaskStructure(**t) for t in tasks_from_file]
+            except FileNotFoundError:
+                return []
+            except json.JSONDecodeError:
+               print("File is empty ")
+               return []
+
+
+    def add_task(self):
+        while True:
+            title = input("Enter the title of the task: ")
+            if title:
                 break
-        except ValueError:
-            print("Please enter a valid priority [0 - 5]")
-    task = {"title": title, "description": description, "priority": priority, "status": "To-Do"}
-    tasks.append(task)
-    print("Task added : "+task["title"])
-    print(" please save the task to not loss it ")
+            print("Please enter a valid title.")
+        while True:
+            description = input("Enter the description of the task: ")
+            if description:
+                break
+            print("Please enter a valid description.")
+        while True:
+            try:
+                priority = int(input("Enter priority , [highest : 0 - lowest : 5] "))
+                if 0 <= priority <= 5:
+                    break
+            except ValueError:
+                print("Please enter a valid priority [0 - 5]")
+ #NEW
+        task = TaskStructure(title,description,priority)
+        self.tasks.append(task)
+        print("Task added : "+task.title ,f"ID: {task.id}")
+        print(" please save the task to not loss it ")
 
 
-def view_tasks(tasks):
-    try:
-        with open("tasks.json", "r") as file:
-            tasks = json.load(file)
-    except Exception:
-        print("No tasks found.")
+    def view_tasks(self):
+        if len(self.tasks)==0:
+            print("No tasks added yet !")
 
-    if len(tasks)==0:
-        print("No tasks added yet !")
+        for i in self.tasks:
+            print(
+                f"ID: {i.id}\n  Title: {i.title}\n  Priority: {i.priority}\n  Status: {i.status}\n"
+            )
+        print("End of Tasks")
 
-    for i in range(len(tasks)):
-        task = tasks[i]
-        print(f"{i + 1} . " + task['title'] + " - " + "the status is : " + task['status'])
+    #for the searching by ID
+    def find_task_by_id(self,task_id):
+        for i in self.tasks:
+            if i.id == task_id:
+                return i
+        print("Task not found ")
+        return None
 
-    print("End of Tasks")
-
-
-# I face a problem with duplication values when save to the file ,with a previous version of code , use this approach to ensure no duplication
-def save():
-    try:
-        with open("tasks.json", "w") as file:
-            json.dump(tasks, file, indent=4)
-        print("Tasks saved successfully")
-    except Exception:
-        print("Error during saving:")
-
-
-
-#  use this function for the sort , change status , delete , mark as done functions
-def load_data():
-    try:
-        with open("tasks.json", "r") as file:
-            tasks_from_file = json.load(file)
-            return tasks_from_file
-    except Exception:
-        print("No tasks found in the list ")
-        return []
+    # I face a problem with duplication values when save to the file ,with a previous version of code , use this approach to ensure no duplication
+    def save(self):
+        try:
+            with open("tasks.json", "w") as file:
+                json.dump([t.as_dictionary() for t in self.tasks], file, indent=4)
+            print("Tasks saved successfully")
+        except FileNotFoundError:
+            print("Error during saving:")
 
 
-
-def sort_tasks():
-    tasks_data = load_data()
-    tasks_data.sort(key=lambda data:data ['priority'])
-    for x in tasks_data:
-        print(x)
-
-
-def mark_as_done():
-    tasks_data_mark = load_data()
-    task_name=input("Enter the task name: ")
-    for task in tasks_data_mark:
-        if task['title'] == task_name:
-            task['status'] = "Done"
-    print(task_name +" : marked as Done")
-
-    with open("tasks.json", "w") as file:
-        json.dump(tasks_data_mark, file, indent=4)
-    return
+    def sort_tasks (self):
+        self.tasks.sort(key=lambda data:data.priority)
+        for i in self.tasks:
+            print(i.title +" with priority :" + f"{i.priority}")
+        print("Tasks sorted successfully")
 
 
-def change_priority():
-    tasks_data_priority = load_data()
-    task_name = input("Enter the task name: ")
-    for task in tasks_data_priority:
-        if task['title'] == task_name:
+    def mark_as_done(self):
+        task_id=int(input("Enter the task id: "))
+        task=self.find_task_by_id(task_id)
+        for task in self.tasks:
+            if task.id == task_id:
+                task.status= "Done"
+        print(task.title+" : marked as Done")
+        self.save()
+
+
+
+
+    def change_priority(self):
+        task_id=int(input("Enter the task id: "))
+        task = self.find_task_by_id(task_id)
+        if task:
             while True:
                 try:
-                    priority1 = int(input("Enter priority , [highest : 0 - lowest : 5] "))
-                    if 0 <= priority1 <= 5:
+                    new_priority = int(input("Enter new priority [0-5]: "))
+                    if 0 <= new_priority <= 5:
+                        task.priority = new_priority
+                        print(" Priority updated.")
                         break
+                    else:
+                        print("Please enter a valid priority [0 - 5]")
                 except ValueError:
-                    print("Please enter a valid priority [0 - 5]")
-            task['priority'] = priority1
-            print(task_name +" :priority  changed successfully")
-
-    with open("tasks.json", "w") as file:
-        json.dump(tasks_data_priority, file, indent=4)
+                    print("Please enter a number.")
+        self.save()
 
 
 
-def delete_task():
-    tasks_data = load_data()
-    task_name = input("Enter the task name: ")
-    for task in tasks_data:
-        if task['title'] == task_name:
-            tasks_data.remove(task)
-            print(task_name + " deleted successfully")
-    with open("tasks.json", "w") as file:
-        json.dump(tasks_data, file, indent=4)
+    def delete_task(self):
+        task_id = input("Enter the task ID: ").strip()
+        task=self.find_task_by_id(task_id)
+        if task:
+            self.tasks.remove(task)
+            print(task.title + " deleted successfully")
+        self.save()
+
+
 
 def main():
-    # this fix the problem I face , when loosing data after stop and rerun code , so now keep the previous data stored
-    global tasks
-    tasks =load_data()
+    TaskOP = TasksOperations()
     while True:
         menu()
         feature= input("Enter your choice please : ")
         match feature:
             case "1":
                 print("Add Task")
-                add_task()
+                TaskOP.add_task()
             case "2":
                 print("View Tasks")
-                view_tasks(tasks)
+                TaskOP.view_tasks()
             case "3":
                 print("Sort Tasks")
-                sort_tasks()
+                TaskOP.sort_tasks()
             case "4":
                 print("Mark_As_Done")
-                mark_as_done()
+                TaskOP.mark_as_done()
             case "5":
                 print("Change priority")
-                change_priority()
+                TaskOP.change_priority()
             case "6":
                 print("Delete Task")
-                delete_task()
+                TaskOP.delete_task()
             case "7":
                 print("Exit")
                 exit()
             case "8":
                 print("Save the list")
-                save()
+                TaskOP.save()
 if __name__ == "__main__":
     main()
 
